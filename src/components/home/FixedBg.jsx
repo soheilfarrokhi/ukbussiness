9dff063ca286d19d83f7f832e125afa6fb899a0d
farrokhdev <farrokhdevs@gmail.com>
2023-08-29 13:13:02 +0330

    updated

--- src/components/home/FixedBg.jsx
+++ src/components/home/FixedBg.jsx
@@ -4,7 +4,8 @@ import FixedBack from "../../assets/images/home/fixed-bg.jpg";
 export const FixedBg = () => {
   return (
     <div
-      className={`w-full bg-[url(./fixed-bg.jpg)] bg-fixed bg-no-repeat bg-center bg-cover w-full h-[500px]`}
+      // className={`w-full bg-[url(./fixed-bg.jpg)] bg-fixed bg-no-repeat bg-center bg-cover w-full h-[500px]`}
+      className={`w-full bg-[url(https://main--meek-nasturtium-d22cd5.netlify.app/fixed-bg.jpg)] bg-fixed bg-no-repeat bg-center bg-cover w-full h-[500px]`}
     ></div>
   );
 };
